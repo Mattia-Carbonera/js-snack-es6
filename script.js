a63b118ccc72336066c48b7b42f6440b1537f95c
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-10-25 17:12:31 +0200

    snack 1 & snack 2

--- script.js
+++ script.js
@@ -20,21 +20,21 @@ const biciclette = [
     },
   },
   {
-    nome: "montainbike",
-    weight: {
-      numWeight: 25,
-      unit: "Kg",
-    },
-  },
-  {
-    nome: "citybike",
+    name: "citybike",
     weight: {
       numWeight: 15,
       unit: "Kg",
     },
   },
   {
-    nome: "racebike",
+    name: "montainbike",
+    weight: {
+      numWeight: 25,
+      unit: "Kg",
+    },
+  },
+  {
+    name: "racebike",
     weight: {
       numWeight: 10,
       unit: "Kg",
@@ -42,7 +42,30 @@ const biciclette = [
   },
 ];
 
-console.log(biciclette);
+// peso bici
+const bikeWeight = biciclette[0].weight.numWeight;
+const montainbikeWeight = biciclette[1].weight.numWeight;
+const citybikeWeight = biciclette[2].weight.numWeight;
+const racebikeWeight = biciclette[3].weight.numWeight;
+const bicycleWeight = [
+  bikeWeight,
+  montainbikeWeight,
+  citybikeWeight,
+  racebikeWeight,
+];
+
+let lightestBike = "";
+
+let currentBike = 0;
+
+for (let i = 0; i < bicycleWeight.length; i++) {
+  if (bicycleWeight[i] > currentBike) {
+    currentBike = bicycleWeight[i];
+    lightestBike = biciclette[i].name;
+  }
+}
+
+console.log("Snack 1: " + lightestBike);
 
 // Snack 2
 // Creare un array di oggetti di squadre di calcio.
@@ -50,6 +73,47 @@ console.log(biciclette);
 // Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+function randomNumberGenerator(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const footballTeam = [
+  {
+    name: "Inter",
+    score: randomNumberGenerator(4),
+    fauls: randomNumberGenerator(8),
+  },
+  {
+    name: "Milan",
+    score: randomNumberGenerator(4),
+    fauls: randomNumberGenerator(8),
+  },
+  {
+    name: "Juventus",
+    score: randomNumberGenerator(4),
+    fauls: randomNumberGenerator(8),
+  },
+  {
+    name: "Roma",
+    score: randomNumberGenerator(4),
+    fauls: randomNumberGenerator(8),
+  },
+  {
+    name: "Lazio",
+    score: randomNumberGenerator(4),
+    fauls: randomNumberGenerator(8),
+  },
+];
+
+const teamName = [];
+
+for (let i = 0; i < footballTeam.length; i++) {
+  teamName.push(footballTeam[i].name);
+  teamName.push(footballTeam[i].fauls);
+}
+
+console.log("Snack 2: " + teamName);
 
 // Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
@@ -70,4 +134,4 @@ function selectNumbers(arrayNumber, highNum, lowNum) {
 }
 
 selectNumbers(num, a, b);
-console.log(numDue);
+console.log("Snack 3: " + numDue);
