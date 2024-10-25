764d7fc979e9897f964bda13706daa1829f2c3ff
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-10-25 15:47:28 +0200

    Snack 3

--- script.js
+++ script.js
@@ -54,3 +54,20 @@ console.log(biciclette);
 // Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const a = 2;
+const b = 6;
+
+let numDue = [];
+
+function selectNumbers(arrayNumber, highNum, lowNum) {
+  for (const arrayNumber of num) {
+    if (highNum < arrayNumber && lowNum > arrayNumber) {
+      numDue.push(arrayNumber);
+    }
+  }
+}
+
+selectNumbers(num, a, b);
+console.log("ciao: " + numDue);
